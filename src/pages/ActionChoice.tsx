fd87d109be1c28103dfba1d2a1d3685c381d6219
risa-h1108 <tusen.takk39@gmail.com>
2026-06-10 00:12:11 +0900

    style:tailwindcssのインストール、CSS当てる動作確認

--- src/pages/ActionChoice.tsx
+++ src/pages/ActionChoice.tsx
@@ -3,19 +3,19 @@
 export default function ActionChoice() {
   return (
     <div>
-      <h1>今、何をやりたい？</h1>
+      <h1 className="text-red-400">今、何をやりたい？</h1>
       <h2>行動を選択してください</h2>
 
-      <div>勉強する</div>
-      <div>運動する</div>
-      <div>掃除する</div>
-      <div>風呂入る</div>
-      <div>その他</div>
+      <div className="rounded-lg">勉強する</div>
+      <div className="rounded-lg">運動する</div>
+      <div className="rounded-lg">掃除する</div>
+      <div className="rounded-lg">風呂入る</div>
+      <div className="rounded-lg">その他</div>
 
       <h2>やる？やらない？</h2>
 
-      <div>やる</div>
-      <div>やらない</div>
+      <div className="rounded-lg">やる</div>
+      <div className="rounded-lg">やらない</div>
     </div>
   );
 }
