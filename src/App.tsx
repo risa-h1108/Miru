b9a8db14088e467f0b6177955d0d489df08806b3
risa-h1108 <tusen.takk39@gmail.com>
2026-06-09 15:00:15 +0900

    init: プロジェクト初期設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
-  const router = BrowserRouter();
-  const nextHandle = () => {
-    router.push("/actionChoice");
-  };
+  //   const router = BrowserRouter();
+  //   const nextHandle = () => {
+  //     router.push("/actionChoice");
+  //   };
 
   return (
     <div>
-      <button onClick={nextHandle}>ダッシュボードへ移動</button>
+      {/* <button onClick={nextHandle}>ダッシュボードへ移動</button> */}
       Miru
     </div>
   );
