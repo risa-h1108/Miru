66d096d78c1fa2ead5ce8fe7ddef99af7e0ab5af
risa-h1108 <tusen.takk39@gmail.com>
2026-06-10 17:34:29 +0900

    style:アイコンの表示、カード表示、各カード位置の調整などのCSS変更

--- src/pages/ActionChoice.tsx
+++ src/pages/ActionChoice.tsx
@@ -1,21 +1,37 @@
-// import App  from "../App";
+import { Icon } from "@iconify/react";
 
 export default function ActionChoice() {
   return (
     <div>
-      <h1 className="text-red-400">今、何をやりたい？</h1>
+      <h1>今、何をやりたい？</h1>
       <h2>行動を選択してください</h2>
 
-      <div className="rounded-lg">勉強する</div>
-      <div className="rounded-lg">運動する</div>
-      <div className="rounded-lg">掃除する</div>
-      <div className="rounded-lg">風呂入る</div>
-      <div className="rounded-lg">その他</div>
+      {/*4つ全てを縦横2列ずつ・中央寄せ */}
+      <div className="grid grid-cols-2 gap-3 px-12 mb-3 mt-3">
+        {/* 各カードやテキストの位置、サイズ */}
+        <div className="border border-gray-500 rounded-lg w-40 h-31.5 flex flex-col items-center justify-center">
+          <Icon icon="lucide:book"></Icon>
+          勉強する
+        </div>
+        <div className="border border-gray-500 rounded-lg w-40 h-31.5">
+          運動する
+        </div>
+        <div className="border border-gray-500 rounded-lg w-40 h-31.5">
+          掃除する
+        </div>
+        <div className="border border-gray-500 rounded-lg w-40 h-31.5">
+          風呂入る
+        </div>
+      </div>
+
+      <div className="border border-gray-500 rounded-lg w-40 h-31.5 mb-3 justify-center">
+        その他
+      </div>
 
       <h2>やる？やらない？</h2>
 
-      <div className="rounded-lg">やる</div>
-      <div className="rounded-lg">やらない</div>
+      <div className="border border-gray-500 rounded-lg">やる</div>
+      <div className="border border-gray-500 rounded-lg">やらない</div>
     </div>
   );
 }
