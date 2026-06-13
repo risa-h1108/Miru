be433267e4a0359c2ee7bb1f70b8fa7bba9f723a
risa-h1108 <tusen.takk39@gmail.com>
2026-06-13 15:37:53 +0900

    style:カードの色変更、CSS完成

--- src/pages/ActionChoice.tsx
+++ src/pages/ActionChoice.tsx
@@ -20,21 +20,21 @@ export default function ActionChoice() {
       {/*4つ全てを縦横2列ずつ・中央寄せ */}
       <div className="grid grid-cols-2 gap-x-4 gap-y-4 px-6 mb-4 mt-3 max-w-sm mx-auto">
         {/* 各カードやテキストの位置、サイズ */}
-        <div className="border border-gray-500 rounded-lg w-40 h-31.5 flex flex-col items-center justify-center text-lg bg-emerald-100 hover:bg-emerald-200">
+        <div className="border border-gray-500 rounded-lg w-40 h-31.5 flex flex-col items-center justify-center text-lg  bg-gray-100 hover:bg-gray-200">
           <Icon icon="lucide:book" width={70} height={70}></Icon>
           勉強する
         </div>
 
-        <div className="border border-gray-500 rounded-lg w-40 h-31.5 flex flex-col items-center justify-center bg-orange-100 hover:bg-orange-200">
+        <div className="border border-gray-500 rounded-lg w-40 h-31.5 flex flex-col items-center justify-center bg-gray-100 hover:bg-gray-200">
           <Icon icon="lucide:dumbbell" width={70} height={70}></Icon>
           運動する
         </div>
 
-        <div className="border border-gray-500 rounded-lg w-40 h-31.5 flex flex-col items-center justify-center  bg-fuchsia-100 hover:bg-fuchsia-200">
+        <div className="border border-gray-500 rounded-lg w-40 h-31.5 flex flex-col items-center justify-center  bg-gray-100 hover:bg-gray-200">
           <Icon icon="lucide-lab:broom" width={70} height={70}></Icon>
           掃除する
         </div>
-        <div className="border border-gray-500 rounded-lg w-40 h-31.5 flex flex-col items-center justify-center bg-cyan-100 hover:bg-cyan-200">
+        <div className="border border-gray-500 rounded-lg w-40 h-31.5 flex flex-col items-center justify-center bg-gray-100 hover:bg-gray-200">
           <Icon icon="lucide-lab:bath-bubble" width={70} height={70}></Icon>
           風呂入る
         </div>
