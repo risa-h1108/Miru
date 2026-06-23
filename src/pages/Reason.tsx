7282dc3d9597c59f3cc32d5206ddea87037f24f1
risa-h1108 <tusen.takk39@gmail.com>
2026-06-23 22:05:04 +0900

    feat:理由選択のテキストとCSSの表示追加

--- src/pages/Reason.tsx
+++ src/pages/Reason.tsx
@@ -3,5 +3,16 @@
 import { Link } from "react-router-dom";
 
 export default function Reason() {
-  return <Link to="/reflection" />;
+  return (
+    <div>
+      <div className=" max-w-sm mx-auto mt-3 ">
+        <h1 className="text-[24px] text-center">やらない理由は？</h1>
+        <h2 className="text-[16px] text-center">
+          当てはまるものを選んでください（複数選択OK）
+        </h2>
+      </div>
+
+      <Link to="/reflection" />
+    </div>
+  );
 }
