111377ab966e14c4a194df8112766661780adf1f
risa-h1108 <tusen.takk39@gmail.com>
2026-06-24 22:33:49 +0900

    feat:理由カードの選択肢一覧を追加

--- src/pages/Reason.tsx
+++ src/pages/Reason.tsx
@@ -1,6 +1,22 @@
 //理由選択画面
 
 import { Link } from "react-router-dom";
+import type { Cards } from "../types";
+
+// 理由カードの選択肢一覧
+const reasonList: Cards[] = [
+  { id: "tired", label: "疲れている", icon: "lucide:bed" },
+  { id: "hassle", label: "面倒くさい", icon: "lucide:annoyed" },
+  { id: "worried", label: "不安がある", icon: "fluent:thinking-20-regular" },
+  { id: "no-time", label: "時間がない", icon: "lucide:alarm-clock-off" },
+  {
+    id: "priorities",
+    label: "他のことを優先したい",
+    icon: "lucide:briefcase",
+  },
+  { id: "question", label: "やり方が分からない", icon: "lucide:circle-help" },
+  { id: "other", label: "その他", icon: "lucide:circle-ellipsis" },
+];
 
 export default function Reason() {
   return (
