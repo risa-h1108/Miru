a974b713ea7841127ac66901fe17102eb8b8a53e
risa-h1108 <tusen.takk39@gmail.com>
2026-06-25 17:46:26 +0900

    feat:理由選択カードをクリックした時の処理の実装、選択中の理由を管理するstateの追加、選択カードを識別する判定式の追加、理由選択カードのmap処理の追加、理由選択の共通CSS追加

--- src/pages/Reason.tsx
+++ src/pages/Reason.tsx
@@ -2,6 +2,12 @@
 
 import { Link } from "react-router-dom";
 import type { Cards } from "../types";
+import { Icon } from "@iconify/react";
+import { useState } from "react";
+
+//共通のCSS（カードの形やサイズ、カード内の位置）
+const reasonCardsBase =
+  "border border-gray-500 rounded-lg w-87 h-22 flex flex-col text-center text-lg";
 
 // 理由カードの選択肢一覧
 const reasonList: Cards[] = [
@@ -18,7 +24,22 @@ const reasonList: Cards[] = [
   { id: "other", label: "その他", icon: "lucide:circle-ellipsis" },
 ];
 
-export default function Reason() {
+export default function ReasonChoice() {
+  // 選択中の理由を管理するstate
+  const [selectedReason, setSelectedReason] = useState<string | null>(null);
+
+  // 理由選択カードをクリックした時の処理
+  const handleCardClick = (label: string) => {
+    setSelectedReason(label); //stateに保存する
+    console.log("clicked", label);
+  };
+
+  //「このカードが選ばれているか」の判定式：「selectedReasonに保存された”選択中のラベル”」と、「今mapが処理しているカードのラベル」が同じか確認。
+  const getReasonCardsBg = (label: string) =>
+    selectedReason === label
+      ? "bg-blue-300" //一致(選択済み)の場合
+      : "bg-blue-100 hover:bg-blue-200"; //不一致（未選択）の場合
+
   return (
     <div>
       <div className=" max-w-sm mx-auto mt-3 ">
@@ -26,6 +47,22 @@ export default function Reason() {
         <h2 className="text-[16px] text-center">
           当てはまるものを選んでください（複数選択OK）
         </h2>
+      </div>
+
+      <div>
+        {reasonList.map((item) => (
+          <div
+            key={item.id}
+            onClick={() => handleCardClick(item.label)} //カードと関数を繋げる
+            className={`${reasonCardsBase} ${getReasonCardsBg(item.label)}`}
+          >
+            {/* アイコンの表示 */}
+            <Icon icon={item.icon} width={40} height={40} />
+
+            {/* ラベル（「勉強する」など）の表示 */}
+            {item.label}
+          </div>
+        ))}
       </div>
 
       <Link to="/reflection" />
