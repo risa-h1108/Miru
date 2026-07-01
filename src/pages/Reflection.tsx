bb398d9179fc5624dca750cc036c61d05cdc09ed
risa-h1108 <tusen.takk39@gmail.com>
2026-07-01 19:06:05 +0900

    style:タイトルを表示

--- src/pages/Reflection.tsx
+++ src/pages/Reflection.tsx
@@ -2,5 +2,12 @@
 import { Link } from "react-router-dom";
 
 export default function Reflection() {
-  return <Link to="/analysis" />;
+  return (
+    <div>
+      <div className=" max-w-sm mx-auto mt-3 ">
+        <h1 className="text-[24px] text-center">振り返り</h1>
+      </div>
+      <Link to="/analysis" />
+    </div>
+  );
 }
