f8c5e793b753a9e4ab05dbed5748a312ff7b1df0
risa-h1108 <tusen.takk39@gmail.com>
2026-07-11 22:39:58 +0900

    style＆refactor:記録表示の位置調整・枠組みを作成、理由項目のみ2行以上時にぶら下げインデントになるように変更

--- src/pages/Reflection.tsx
+++ src/pages/Reflection.tsx
@@ -1,6 +1,10 @@
 //振り返り画面
 import { Link, useLocation } from "react-router-dom";
 
+//記録内容ボックスのCSS
+const recordBase =
+  "w-full max-w-sm mx-auto border border-gray-400 rounded-lg mt-6 p-5 text-xl space-y-1";
+
 export default function Reflection() {
   const location = useLocation();
   const selectedAction = location.state?.selectedAction ?? "";
@@ -20,12 +24,17 @@ export default function Reflection() {
         <h2 className="text-[16px] text-center">結果を記録しましょう</h2>
       </div>
 
-      <div>
+      <div className={recordBase}>
         <p>行動：{selectedAction}</p>
         {/*[true/false]で表示されるのを日本語の文字列に変換してから表示する */}
         <p>選択：{selectedDecision ? "やる" : "やらない"}</p>
-        {/*.join(区切り文字)：配列の中身(selectedReasons)を指定した区切り文字(、)で繋げて、1本の文字列に変更する */}
-        <p>理由：{selectedReasons.join("、")}</p>
+        {/* flex：「理由：」と値を横並びにする、shrink-0：ラベル部分(「理由：」)の幅を縮めさせない。
+            flex + shrink-0 = ぶら下げインデント */}
+        <div className="flex">
+          <span className="shrink-0">理由：</span>
+          {/*.join(区切り文字)：配列の中身(selectedReasons)を指定した区切り文字(、)で繋げて、1本の文字列に変更する */}
+          <span>{selectedReasons.join("、")}</span>
+        </div>
         <p>日時：{recordedAt}</p>
       </div>
 
