4aff4dc612a79b7aae6055e0c136e520239768c2
risa-h1108 <tusen.takk39@gmail.com>
2026-07-26 15:44:53 +0900

    feat:既存ページにて、タブバー付きの見た目にするコンポーネントを追加実装

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,17 @@ import Home from "./pages/Home";
 import Reflection from "./pages/Reflection";
 import Analysis from "./pages/Analysis";
 import ReasonsChoice from "./pages/ReasonsChoice";
+import LayoutWithTabBar from "./components/LayoutWithTabBar";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
+      {/* 下記のページにて、タブバー付きの見た目にする */}
+      <Route element={<LayoutWithTabBar />} />
+
+      {/* 各ページ */}
       <Route path="/action" element={<ActionChoice />} />
       <Route path="reasons" element={<ReasonsChoice />} />
       <Route path="reflection" element={<Reflection />} />
